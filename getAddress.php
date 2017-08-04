743f3da5b6b5f450a687f51738a6fde5d368e5dd
Michael Demetriou <qwazix@gmail.com>
2017-08-04 14:30:38 +0300

    Lock wallet after creating new address and validate new address with correct wallet make address used (was commented)

--- getAddress.php
+++ getAddress.php
@@ -7,7 +7,7 @@ if(isset($_GET['hisAddress'])){
     $hisAddress=$_GET['hisAddress'];
     if (validate($hisAddress)){
         $ourAddress = $oldWallet->getaddressesbyaccount("hisAddress=".$hisAddress)[0];
-        if (trim($ourAddress)=="" || !validate($ourAddress)){
+        if (trim($ourAddress)=="" || !validate($ourAddress, false)){
             try{
                 if ($walletpassphrase_old!="") $oldWallet->walletpassphrase($walletpassphrase_old,30);
     //            echo $newaddress = $wallet->getaccountaddress("");
@@ -15,6 +15,7 @@ if(isset($_GET['hisAddress'])){
     //            6Jpr3q6xzim6HG5GWCYjpMjima9BfBbCVm
                 $result = "ok";
                 $ourAddress = $oldWallet->getnewaddress("hisAddress=".$hisAddress);
+                if ($walletpassphrase_old!="") $oldWallet->walletlock();
             } catch (Exception $e) {
                 $result = "norpc";
                 //TODO send mail.
